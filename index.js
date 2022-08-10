56425ea21cc37fbc8c66d5c8d89702ce5cc3fe57
Misha Tuesday <mishatuesday@gmail.com>
2022-08-10 15:25:30 -0400

    commit tree 6c786f21a81818985e08d55bdeb2224ed1023f25

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
+countDown(76);
+
+function writeCards() {
+    
+}
